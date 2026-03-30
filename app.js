b61e3c87134cffc9e9970f9cb76fc8429ca7b9f8
Renda Mudau <107690881+404-heisenberg@users.noreply.github.com>
2026-03-30 13:31:19 +0200

    event listener to handle checkbox and delete buttons

--- app.js
+++ app.js
@@ -10,3 +10,42 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   renderTask();
 });
+
+// mark a task as completed when checkbox is clicked
+taskList.addEventListener("click", (e) => {
+  let button = e.target.closest("button");
+  if (!button) return;
+
+  let taskElement = button.closest("li");
+  if (!taskElement) return;
+
+  let task = tasks.find((t) => t.id === taskElement.dataset.taskId);
+  if (!task) return;
+
+  // if the checkbox is clicked, toggle the completed class and change the image
+  if (button.classList.contains("toggle-btn")) {
+    // mark the task as completed if it isn't completed already
+    if (!task.completed) {
+      task.completed = true;
+      taskElement.classList.add("completed");
+    } else if (task.completed) {
+      task.completed = false;
+      taskElement.classList.remove("completed");
+    }
+    const img = button.querySelector("img");
+    if (img) {
+      img.src = task.completed
+        ? "images/completed-checkbox.svg"
+        : "images/checkbox.svg";
+    }
+  }
+
+  // if the trash is clicked, delete the task
+  if (button.classList.contains("delete-btn")) {
+    // delete the task
+    taskElement.remove();
+    const taskIndex = tasks.indexOf(task);
+    if (taskIndex === -1) return;
+    tasks.splice(taskIndex, 1);
+  }
+});
